3e13d246a811beb6fe35ed4bb280829fd12569e4
Yaroslav Myaskovsky <yaroslav.myaskovsky@gmail.com>
2020-07-28 18:44:34 +0300

    Just copied reset password func from flask blog.

--- app/users/forms.py
+++ app/users/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from app.models import User
 
 
 class SignUpForm(FlaskForm):
@@ -17,3 +18,19 @@ class SignInForm(FlaskForm):
     password = PasswordField(validators=[DataRequired()], render_kw={"placeholder": "Password"})
     remember = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+
+class RequestResetForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()],  render_kw={"placeholder": "Email"})
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with that email. You must register first.')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()], render_kw={"placeholder": "Password"})
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')], render_kw={"placeholder": "Confirm password"})
+    submit = SubmitField('Reset Password')
--- app/users/routes.py
+++ app/users/routes.py
@@ -3,9 +3,8 @@ from app.users.forms import SignInForm, SignUpForm
 from flask_login import login_user, current_user, logout_user, login_required
 from app import db, bcrypt
 from app.models import User, Post
-from app.users.forms import (SignUpForm, SignInForm)#, UpdateAccountForm,
-                                  #RequestResetForm, ResetPasswordForm)
-#from app.users.utils import save_picture, send_reset_email
+from app.users.forms import (SignUpForm, SignInForm, RequestResetForm, ResetPasswordForm)
+from app.users.utils import  send_reset_email#, save_picture
 
 users = Blueprint('users', __name__)
 
@@ -46,3 +45,34 @@ def sign_in():
 def sign_out():
     logout_user()
     return redirect(url_for('main.home'))
+
+
+@users.route("/reset_password", methods=['GET', 'POST'])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+    form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        send_reset_email(user)
+        flash('An email has been sent with instructions to reset your password.', 'info')
+        return redirect(url_for('users.sign_in'))
+    return render_template('public/reset_request.html', title='Reset Password', form=form)
+
+
+@users.route("/reset_password/<token>", methods=['GET', 'POST'])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+    user = User.verify_reset_token(token)
+    if user is None:
+        flash('That is an invalid or expired token', 'warning')
+        return redirect(url_for('users.reset_request'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user.password = hashed_password
+        db.session.commit()
+        flash('Your password has been updated! You are now able to log in', 'success')
+        return redirect(url_for('users.sign_in'))
+    return render_template('public/reset_token.html', title='Reset Password', form=form)
